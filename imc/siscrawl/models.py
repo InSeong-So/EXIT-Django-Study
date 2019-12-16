a942ee02be9308f63e3f0b937bf640934a3cc7d2
InSeong-So <goflvhxj2547@gmail.com>
2019-12-16 22:51:28 +0900

    에러 해결

--- imc/siscrawl/models.py
+++ imc/siscrawl/models.py
@@ -1,5 +1,12 @@
-# from django.db import models
-#
-# class SaraminMaster(models.Model):
-#     SEQ_NO = models.BigAutoField()
-#     COMPANY_NM = models.CharField(max_length=200)
+from django.db import models
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
